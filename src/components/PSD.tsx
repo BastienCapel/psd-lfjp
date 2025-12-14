678572df29cc024cfc141263e41d691da5aacc70
CAPEL Bastien <capel.direction@gmail.com>
2025-12-14 09:24:45 +0000

    Remove PSD intro section

--- src/components/PSD.tsx
+++ src/components/PSD.tsx
@@ -1,17 +1,11 @@
 
 import React from 'react';
-import PSDIntroduction from './PSDIntroduction';
 import PSDTabs from './PSDTabs';
 
 const PSD = () => {
   return <section id="psd" className="py-16 md:py-24 bg-gray-50">
       <div className="container mx-auto px-6">
-        <div className="max-w-4xl mx-auto">
-          <h2 className="text-3xl font-playfair font-bold text-french-blue mb-6 text-center">
-            Plan stratégique de développement du Lycée Français Jacques Prévert de Saly
-          </h2>
-          
-          <PSDIntroduction />
+        <div className="max-w-5xl mx-auto">
           <PSDTabs />
         </div>
       </div>
